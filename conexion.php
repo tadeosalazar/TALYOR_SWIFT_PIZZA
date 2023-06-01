cedc28a8c17f08624faadfcd88306c6b1a0042ee
EmilioGonzalez08 <emilioandres09@gmail.com>
2023-05-31 23:38:52 -0600

    base de datos php (registro)

---
+++ conexion.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Regístrate Ahora</title>
+    <link rel="stylesheet" type="text/css" href="CSS/registro.css">
+</head>
+<body>
+    <?php
+    $enlace = mysqli_connect("localhost","root", "", "swiftpizza");
+
+    if(!$enlace){
+        die("no pudo conectarse a la base de datos ". mysqli_error());
+    }
+    echo "";
+    mysqli_close($enlace);
+    ?>
+    <form action="#" name="swiftpizza" method="post">
+        <h1>Regístrate Ahora</h1>
+        <label for="nombre">Nombre:</label>
+        <input type="nombre" id="nombre" name="nombre" required>
+        <label for="email">Correo electrónico:</label>
+        <input type="email" id="email" name="email" required>
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" name="password" required>
+        
+        <button type="submit" name="registro">Registrarse</button>
+        <input type="reset">
+    </form>
+</body>
+
+<?php
+if(isset($_POST['registro'])){
+    $enlace = mysqli_connect("localhost", "root", "", "swiftpizza");
+
+    if(!$enlace){
+        die("no pudo conectarse a la base de datos " . mysqli_error());
+    }
+
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['email'];
+    $password = $_POST['password'];
+
+    $insertarDatos = "INSERT INTO datos VALUES ('$nombre', '$correo', '$password', '')";
+    $ejecutarInsertar = mysqli_query($enlace, $insertarDatos);
+
+    if($ejecutarInsertar){
+        echo "Los datos se han guardado correctamente.";
+    } else {
+        echo "Error al guardar los datos: " . mysqli_error($enlace);
+    }
+
+    mysqli_close($enlace);
+}
+?>
+
+</html>
